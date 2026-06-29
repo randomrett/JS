0dface9838dbd636aec30372045f55d087a2b2c3
randomrett <chiragzn9784@gmail.com>
2026-06-29 12:37:47 +0000

    comparisons and ===

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+// console.log(2>1)
+// console.log(2==1)
+// console.log(1!=3)
+
+//Avoid these kind of comparisons:
+// console.log(5>"02");
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(undefined>0)
+// console.log(undefined>=0)
+// console.log(undefined==0)
+
+// ===
+console.log("2"===2)
