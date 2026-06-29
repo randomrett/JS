54962e69193ba1d4a7154bba463dff4dc06a3a50
randomrett <chiragzn9784@gmail.com>
2026-06-29 10:40:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "chirag"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1
+// false -> 0
+// undefined -> NaN
+// null -> 0
+
+// let isLoggedIn = "chirag"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // 1 -> true; 0 -> false
+// // "" -> false
+// // "chirag" -> true
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let digit = 33
+let stringDigit = String(digit)
+console.log(stringDigit)
+console.log(typeof stringDigit)
