f5cb01b54c9d6ab0435e50653203111c2e7c1ed4
randomrett <chiragzn9784@gmail.com>
2026-06-29 12:12:41 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,7 +22,36 @@
 // console.log(typeof booleanIsLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-let digit = 33
-let stringDigit = String(digit)
-console.log(stringDigit)
-console.log(typeof stringDigit)
+// let digit = 33
+// let stringDigit = String(digit)
+// console.log(stringDigit)
+// console.log(typeof stringDigit)
+
+
+//*********** OPERATIONS ***********//
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2**2)
+
+// let str1 = "Chirag"
+// let str2 = " Jain"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+ 2 + 2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 11;
+++gameCounter;
+console.log(gameCounter);
+
