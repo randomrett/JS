0bee6a4ab4390da806dee30d38837cb9126d84be
randomrett <chiragzn9784@gmail.com>
2026-07-06 16:58:02 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+const name = "Chirag"
+const repoCount = 3
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`)
+const gameName = new String('chirag+jain+ji')
+// console.log(gameName[6])
+// console.log(gameName.__proto__)
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4))
+// console.log(gameName.indexOf('j'))
+
+// const String1 = gameName.substring(3,7)
+// console.log(String1)
+// const String2 = gameName.slice(-2,4)
+// console.log(String2)
+// const String3 = "    hitesh   "
+// console.log(String3)
+// console.log(String3.trim())
+
+// const url = "https://chirag.com/chirag%21jain";
+// console.log(url.replace('%21','__'));
+
+// console.log(url.includes('jain'))
+console.log(gameName.split('+'));
+
