9ee04e41ad31f8f751818fb7022a75361a44921e
randomrett <chiragzn9784@gmail.com>
2026-07-07 17:11:50 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+// // const score = 399
+// // console.log(score)
+
+// // const marks = new Number(499)
+// // console.log(marks)
+// // console.log(marks.toString().length);
+// // console.log(typeof marks);
+// // console.log(marks.toFixed(2));
+
+// const score2 = 153.2359;
+// console.log(score2.toPrecision(3));  //returns a string
+
+// const score3 = 10230909;
+// console.log(score3.toLocaleString('en-IN'));
+
+
+////***************MATHS*************////
+// console.log(Math)
+// console.log(Math.round(2.9));
+// console.log(Math.min(4,1,9,0));
+
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+
+const max = 20;
+const min = 10;
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
