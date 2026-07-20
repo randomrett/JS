7f0ae1ab1ab4184e1e9694c1f1cbcdd91dc36252
randomrett <chiragzn9784@gmail.com>
2026-07-20 12:54:29 +0000

    for in

---
+++ 05_iterations/twoo.js
@@ -0,0 +1,25 @@
+// const myObj = {
+//     js : 'javascript',
+//     cpp : 'C++',
+//     rb : 'ruby',
+//     swift : 'swift by apple'
+// }
+
+// for (const key in myObj) {
+//     console.log(`'${key}' shortcut is for "${myObj[key]}"`);
+// }
+
+// const myArr = ["js","cpp","rb","python","java"];
+// for(const lang in myArr){
+//     console.log(lang, ':-', myArr[lang]);
+// }
+
+// const map = new Map();
+// map.set('IN',"India");
+// map.set('USA',"United States of America");
+// map.set('FR',"France");
+// map.set('IN',"India");
+
+// for(const key in map){               //// can't iterate with this method in map
+//     console.log(key);
+// }
